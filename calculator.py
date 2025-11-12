0deb6b836e395a7b0908f72dbcbedce47f0cb725
Leonna Xie <197978858+koicaaaa@users.noreply.github.com>
2025-11-12 10:24:26 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -14,9 +14,6 @@ def square_root(a):
 def hypotenuse(a, b):
     return math.hypot(a, b)
 
-# First example
-import math
-
 def add(a, b): 
     return a + b
 
@@ -43,21 +40,5 @@ def log(a,b):
 
 def exp(a,b):
     return a ** b
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def log(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError()
-    if b <= 0:
-        raise ValueError()
-    return math.log(b, a)
-
-def exp(a, b):
-    return math.pow(a, b)
 
 
